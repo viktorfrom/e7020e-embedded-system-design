325bf2825bd9acfa0e35baf0ae8f559b8b4b97e5
Mark Hakansson <mark.hakansson@live.se>
2020-03-09 14:16:33 +0100

    Remove Aron's code in main for now

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-#![cfg_attr(not(test), no_std)]
+//#![cfg_attr(not(test), no_std)]
 #![no_main]
-#![allow(deprecated)]
-
-mod longfi_bindings;
+#![no_std]
+//#![allow(deprecated)]
 
 extern crate panic_semihosting;
 
-use hal::{
+use stm32l0xx_hal as hal;
+use cortex_m::peripheral::DWT;
+use cortex_m_semihosting::hprintln;
+//use rtfm::cyccnt::{Instant, U32Ext as _};
+
+use stm32l0xx_hal::{
+    adc,
     exti::TriggerEdge,
     gpio::*,
     pac,
@@ -14,39 +19,35 @@ use hal::{
     rcc::Config,
     spi,
     syscfg,
+    stm32
 };
-use longfi_bindings::AntennaSwitches;
-use longfi_device;
-use longfi_device::LongFi;
-use longfi_device::{ClientEvent, RfConfig, RfEvent};
-use stm32l0xx_hal as hal;
-use communicator::{Message, Channel};
-use heapless::consts::*;
 
 #[rtfm::app(device = stm32l0xx_hal::pac, peripherals = true)]
 const APP: () = {
-
     struct Resources {
-        INT: pac::EXTI,
-        SX1276_DIO0: gpiob::PB4<Input<PullUp>>,
+        INT: stm32::EXTI,
         BUTTON: gpioa::PA4<Input<PullUp>>,
+        BUZZER: gpioa::PA3<Output<PushPull>>,
+        #[init(false)]
+        BUZZER_ON: bool,
         #[init([0; 512])]
         BUFFER: [u8; 512],
-        LONGFI: LongFi,
-        LED: gpiob::PB2<Output<PushPull>>,
         #[init(false)]
-        STATE: bool,
-        #[init(0)]
-        COUNTER_1: u32,
-        #[init(0)]
-        COUNTER_2: u32
+        STATE: bool
     }
 
-    #[init(resources = [BUFFER])]
+    #[init]
     fn init(cx: init::Context) -> init::LateResources {
         // Configure the clock.
         let mut rcc = cx.device.RCC.freeze(Config::hsi16());
         let mut syscfg = syscfg::SYSCFG::new(cx.device.SYSCFG, &mut rcc);
+
+/*         let mut core = cx.core;
+        core.DCB.enable_trace();
+        core.DWT.enable_cycle_counter();
+ */
+        // Configure ADC
+        // let mut adc = adc::Adc::new(cx.device.ADC, &mut rcc);
 
         // Acquire the GPIOB peripheral. This also enables the clock for GPIOB in
         // the RCC register.
@@ -54,134 +55,66 @@ const APP: () = {
         let gpiob = cx.device.GPIOB.split(&mut rcc);
         let gpioc = cx.device.GPIOC.split(&mut rcc);
 
-        let exti = cx.device.EXTI;
+        // Reset button
+        let reset = gpioc.pc0.into_push_pull_output();
 
-        // Configure PB4 as input.
+        // Configure inputs
         let sx1276_dio0 = gpiob.pb4.into_pull_up_input();
-        // Configure PA4 for button input.
         let button = gpioa.pa4.into_pull_up_input();
 
-        // Configure the external interrupt on the falling edge for the pin 2.
+        // External interrupt
+        let exti = cx.device.EXTI;
+
+        // Configure external interrupt for button
         exti.listen(
             &mut syscfg,
-            sx1276_dio0.port(),
-            sx1276_dio0.pin_number(),
-            TriggerEdge::Rising,
+            button.port(),
+            button.pin_number(),
+            TriggerEdge::Falling,  
         );
 
         let sck = gpiob.pb3;
         let miso = gpioa.pa6;
         let mosi = gpioa.pa7;
         let nss = gpioa.pa15.into_push_pull_output();
-        longfi_bindings::set_spi_nss(nss);
 
-        // Initialise the SPI peripheral.
-        let mut _spi = cx.device
-            .SPI1
-            .spi((sck, miso, mosi), spi::MODE_0, 1_000_000.hz(), &mut rcc);
-
-        let reset = gpioc.pc0.into_push_pull_output();
-        longfi_bindings::set_radio_reset(reset);
-
-        let ant_sw = AntennaSwitches::new(
-            gpioa.pa1.into_push_pull_output(),
-            gpioc.pc2.into_push_pull_output(),
-            gpioc.pc1.into_push_pull_output(),
-        );
-
-        longfi_bindings::set_antenna_switch(ant_sw);
-
-        let en_tcxo = gpioa.pa8.into_push_pull_output();
-        longfi_bindings::set_tcxo_pins(en_tcxo);
-
-        static mut BINDINGS: longfi_device::BoardBindings = longfi_device::BoardBindings {
-            reset: Some(longfi_bindings::radio_reset),
-            spi_in_out: Some(longfi_bindings::spi_in_out),
-            spi_nss: Some(longfi_bindings::spi_nss),
-            delay_ms: Some(longfi_bindings::delay_ms),
-            get_random_bits: Some(longfi_bindings::get_random_bits),
-            set_antenna_pins: Some(longfi_bindings::set_antenna_pins),
-            set_board_tcxo: Some(longfi_bindings::set_tcxo),
-        };
-
-        let rf_config = RfConfig {
-            oui: 0xBEEF_FEED,
-            device_id: 0xABCD,
-        };
-
-        let mut longfi_radio = unsafe { LongFi::new(&mut BINDINGS, rf_config).unwrap() };
-
-        longfi_radio.set_buffer(cx.resources.BUFFER);
-
-        longfi_radio.receive();
-
-        // Configure PB5 as output.
-        let mut led = gpiob.pb2.into_push_pull_output();
-        led.set_low().ok();
+        // Configure outputs
+        let mut buzzer = gpioa.pa3.into_push_pull_output();
 
         // Return the initialised resources.
         init::LateResources {
             INT: exti,
-            SX1276_DIO0: sx1276_dio0,
             BUTTON: button,
-            LONGFI: longfi_radio,
-            LED: led,
+            BUZZER: buzzer,
         }
     }
-
-    #[task(capacity = 4, priority = 2, resources = [BUFFER, LONGFI, LED, STATE, COUNTER_1, COUNTER_2])]
-    fn radio_event(cx: radio_event::Context, event: RfEvent) {
-        let longfi_radio = cx.resources.LONGFI;
-        let client_event = longfi_radio.handle_event(event);
-        match client_event {
-            ClientEvent::ClientEvent_TxDone => {
-                longfi_radio.receive();
-            }
-            ClientEvent::ClientEvent_Rx => {
-                let rx_packet = longfi_radio.get_rx();
-
-                {
-                    let buf = unsafe {
-                        core::slice::from_raw_parts(rx_packet.buf, rx_packet.len)
-                    };
-                    let message = Message::deserialize(buf);
-
-                    if let Some(message) = message {
-                        // Let's assume we only have permission to use ID 2:
-                        if message.id != 2 {
-                            longfi_radio.set_buffer(cx.resources.BUFFER);
-                            longfi_radio.receive();
-                            return;
-                        }
-
-                        let binary = application(
-                            message,
-                            cx.resources.COUNTER_1,
-                            cx.resources.COUNTER_2,
-                            cx.resources.STATE,
-                            cx.resources.LED
-                        );
-                        longfi_radio.send(&binary);
-                    }
-                }
-
-                longfi_radio.set_buffer(cx.resources.BUFFER);
-                longfi_radio.receive();
-            }
-            ClientEvent::ClientEvent_None => {}
-        }
-    }
-
-    #[task(binds = EXTI4_15, priority = 1, resources = [SX1276_DIO0, INT], spawn = [radio_event])]
+ 
+    #[task(binds = EXTI4_15, priority = 2, resources = [BUTTON, INT], spawn = [button_event])]
     fn exti4_15(cx: exti4_15::Context) {
-        cx.resources.INT.clear_irq(cx.resources.SX1276_DIO0.pin_number());
-        cx.spawn.radio_event(RfEvent::DIO0).unwrap();
+        cx.resources.INT.clear_irq(cx.resources.BUTTON.pin_number());
+        cx.spawn.button_event().unwrap();        
     }
 
-    
-    fn button_input(cx: button_input::Context) {
-        
+    #[task(priority = 1, resources = [BUZZER])]
+    fn button_event(cx: button_event::Context) {
+        static mut state: bool = true;
+        if *state {
+            hprintln!("set high").unwrap();
+            *state = false;
+        } else {
+            hprintln!("set low").unwrap();
+            *state = true;
+        }
     }
+
+/*     #[]
+    fn buzzer_on(cx: buzzer_on::Context) {
+
+    }
+
+    fn buzzer_off(cx: buzzer_off::Context) {
+
+    } */
 
     // Interrupt handlers used to dispatch software tasks
     extern "C" {
