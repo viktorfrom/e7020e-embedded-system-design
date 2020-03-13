2d1a3ce3c54117643fb41fb8ed92bb57b69595ef
Mark Hakansson <mark.hakansson@live.se>
2020-03-13 15:16:43 +0100

    Add SPI2 connection to oled example

--- examples/oled.rs
+++ examples/oled.rs
@@ -1,170 +1,96 @@
-//! Bounce a DVD player logo around the screen
-//!
-//! Like this, but with no color changing: https://bouncingdvdlogo.com/
-//!
-//! For best results, run with the `--release` flag.
-
 #![no_std]
 #![no_main]
 
-use core::convert::TryFrom;
-use embedded_graphics::{
-    geometry::Point, image::Image, pixelcolor::BinaryColor, prelude::*, primitives::Rectangle,
-};
-use panic_halt as _;
+extern crate panic_semihosting;
+
 use rtfm::app;
 use ssd1306::{prelude::*, Builder};
-use stm32l0xx_hal::{
-    delay::Delay,
-    gpio,
-    pac::{self, SPI1},
-    prelude::*,
-    spi::{self, Mode, Phase, Polarity, Spi},
-    timer::{CountDownTimer, Event, Timer},
-};
-use tinybmp::Bmp;
+use stm32l0xx_hal as hal;
 
-type Display = ssd1306::mode::graphics::GraphicsMode<
-    ssd1306::interface::spi::SpiInterface<
-        spi::Spi<
-            SPI1,
-            spi::Spi1NoRemap,
-            (
-                gpio::gpioa::PA5<gpio::Alternate<gpio::PushPull>>,
-                gpio::gpioa::PA6<gpio::Input<gpio::Floating>>,
-                gpio::gpioa::PA7<gpio::Alternate<gpio::PushPull>>,
-            ),
-        >,
-        gpio::gpiob::PB1<gpio::Output<gpio::PushPull>>,
-    >,
->;
+use hal::{
+    delay::Delay,
+    exti::TriggerEdge,
+    gpio::*,
+    pac,
+    prelude::*,
+    rcc::Config,
+    spi::{self, Mode, Phase, Polarity, NoMiso},
+    syscfg,
+    timer::{Timer},
+};
 
 #[app(device = stm32l0xx_hal::pac, peripherals = true)]
 const APP: () = {
     struct Resources {
-        display: Display,
-        timer: CountDownTimer<pac::TIM1>,
-        top_left: Point,
-        velocity: Point,
-        bmp: Bmp<'static>,
     }
 
     #[init]
-    fn init(cx: init::Context) -> init::LateResources {
-        let dp = cx.device;
-        let core = cx.core;
+    fn init(cx: init::Context) {
+        // Configure the clock.
+        let mut rcc = cx.device.RCC.freeze(Config::hsi16());
+        let mut syscfg = syscfg::SYSCFG::new(cx.device.SYSCFG, &mut rcc);
 
-        let mut flash = dp.FLASH.constrain();
-        let mut rcc = dp.RCC.constrain();
+        // Acquire the GPIOB peripheral. This also enables the clock for GPIOB in
+        // the RCC register.
+        let gpioa = cx.device.GPIOA.split(&mut rcc);
+        let gpiob = cx.device.GPIOB.split(&mut rcc);
+        let gpioc = cx.device.GPIOC.split(&mut rcc);
 
-        let clocks = rcc
-            .cfgr
-            .use_hse(8.mhz())
-            .sysclk(72.mhz())
-            .pclk1(36.mhz())
-            .freeze(&mut flash.acr);
+        let nss = gpiob.pb12.into_push_pull_output();
+        let sck = gpiob.pb13;
+        let mosi = gpiob.pb15;
 
-        let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+        // Initialise the SPI peripheral.
+        let mut spi = cx.device
+           .SPI2
+           .spi(
+                (sck, NoMiso, mosi), 
+                spi::MODE_0, 
+                1_000_000.hz(), 
+                &mut rcc
+            );
 
-        let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
-        let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+        let dc = gpiob.pb8.into_push_pull_output();
+        let res = gpiob.pb9.into_push_pull_output();
 
-        // SPI1
-        let sck = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
-        let miso = gpioa.pa6;
-        let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
+        let mut reset = gpioa.pa3.into_push_pull_output();
+        let mut delay = Delay::new(cx.core.SYST, rcc.clocks);
 
-        let mut delay = Delay::new(core.SYST, clocks);
+        let mut disp: GraphicsMode<_> = Builder::new().connect_spi(spi, dc).into();
 
-        let mut rst = gpiob.pb0.into_push_pull_output(&mut gpiob.crl);
-        let dc = gpiob.pb1.into_push_pull_output(&mut gpiob.crl);
+        disp.reset(&mut reset, &mut delay).unwrap();
+        disp.init().unwrap();
+    
+        // Top side
+        disp.set_pixel(0, 0, 1);
+        disp.set_pixel(1, 0, 1);
+        disp.set_pixel(2, 0, 1);
+        disp.set_pixel(3, 0, 1);
+    
+        // Right side
+        disp.set_pixel(3, 0, 1);
+        disp.set_pixel(3, 1, 1);
+        disp.set_pixel(3, 2, 1);
+        disp.set_pixel(3, 3, 1);
+    
+        // Bottom side
+        disp.set_pixel(0, 3, 1);
+        disp.set_pixel(1, 3, 1);
+        disp.set_pixel(2, 3, 1);
+        disp.set_pixel(3, 3, 1);
+    
+        // Left side
+        disp.set_pixel(0, 0, 1);
+        disp.set_pixel(0, 1, 1);
+        disp.set_pixel(0, 2, 1);
+        disp.set_pixel(0, 3, 1);
+    
+        disp.flush().unwrap();
 
-        let spi = Spi::spi1(
-            dp.SPI1,
-            (sck, miso, mosi),
-            &mut afio.mapr,
-            Mode {
-                polarity: Polarity::IdleLow,
-                phase: Phase::CaptureOnFirstTransition,
-            },
-            8.mhz(),
-            clocks,
-            &mut rcc.apb2,
-        );
-
-        let mut display: GraphicsMode<_> = Builder::new()
-            .with_rotation(DisplayRotation::Rotate180)
-            .connect_spi(spi, dc)
-            .into();
-
-        display.reset(&mut rst, &mut delay).unwrap();
-        display.init().unwrap();
-
-        // Update framerate
-        let fps = 20;
-
-        let mut timer = Timer::tim1(dp.TIM1, &clocks, &mut rcc.apb2).start_count_down(fps.hz());
-
-        timer.listen(Event::Update);
-
-        let bmp = Bmp::from_slice(include_bytes!("dvd.bmp")).unwrap();
-
-        // Init the static resources to use them later through RTFM
-        init::LateResources {
-            timer,
-            display,
-            top_left: Point::new(5, 3),
-            velocity: Point::new(1, 1),
-            bmp,
-        }
-    }
-
-    #[task(binds = TIM1_UP, resources = [display, top_left, velocity, timer, bmp])]
-    fn update(cx: update::Context) {
-        let update::Resources {
-            display,
-            top_left,
-            velocity,
-            timer,
-            bmp,
-            ..
-        } = cx.resources;
-
-        let bottom_right = *top_left + Point::try_from(bmp.dimensions()).unwrap();
-
-        // Erase previous image position with a filled black rectangle
-        Rectangle::new(*top_left, bottom_right)
-            .into_styled(embedded_graphics::primitive_style!(
-                fill_color = BinaryColor::Off
-            ))
-            .draw(display)
-            .unwrap();
-
-        // Check if the image collided with a screen edge
-        {
-            if bottom_right.x > display.size().width as i32 || top_left.x < 0 {
-                velocity.x = -velocity.x;
-            }
-
-            if bottom_right.y > display.size().height as i32 || top_left.y < 0 {
-                velocity.y = -velocity.y;
-            }
-        }
-
-        // Move the image
-        *top_left += *velocity;
-
-        // Draw image at new position
-        Image::new(bmp, *top_left).draw(display).unwrap();
-
-        // Write changes to the display
-        display.flush().unwrap();
-
-        // Clears the update flag
-        timer.clear_update_interrupt_flag();
+        // Return the initialised resources.
     }
 
     extern "C" {
-        fn EXTI0();
+        fn USART4_USART5();    
     }
 };
