59f0fc9ce3106f0241a4103c68b92acf3eea1799
viktorfrom <viktor.from91@gmail.com>
2020-03-22 16:43:30 +0100

    Add mod oled to main

--- src/oled.rs
+++ src/oled.rs
@@ -12,7 +12,7 @@ use stm32l0xx_hal::{
     pac,
     prelude::*,
     rcc::Config,
-    spi::{self, Mode, NoMiso, Phase, Polarity},
+    //spi::{self, Mode, NoMiso, Phase, Polarity},
     syscfg,
     timer::Timer,
 };
@@ -26,30 +26,12 @@ use embedded_graphics::{
 };
 
 pub struct Oled {
-    pub cs: PB12<Input<Floating>>,
-    pub sck: PB13<Input<Floating>>,
-    pub mosi: PB15<Input<Floating>>,
-    pub spi: PB15<Input<Floating>>,
+    pub test: spi,
     pub state: bool,
 }
 
 impl Oled {
-    pub fn new(
-        cs: PB12<Input<Floating>>,
-        sck: PB13<Input<Floating>>,
-        mosi: PB15<Input<Floating>>,
-        cx: cx,
-        rcc: rcc,
-    ) -> Oled {
-        Oled {
-            cs: cs,
-            sck: sck,
-            mosi: mosi,
-            spi: cx
-                .device
-                .SPI2
-                .spi((sck, NoMiso, mosi), spi::MODE_0, 1_000_000.hz(), &mut rcc),
-            state: false,
-        }
+    pub fn new(test: spi) -> Oled {
+        Oled { test: test, state: false }
     }
 }
